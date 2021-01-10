79113d87c450b791545fb9e8ae9f65b43dced9bf
gent20 <gentkastratio@gmail.com>
2021-01-10 13:25:50 +0100

    converted the methods to async and await

--- easyhttp2/easyhttp2.js
+++ easyhttp2/easyhttp2.js
@@ -1,59 +1,48 @@
 class EasyHTTP{
 
-    get(url) {
-        return new Promise((resolve , reject) => {
-            fetch(url)
-                .then(res => res.json())
-                .then(data => resolve(data))
-                .catch(err => reject(err));
-            });
-        }
+   async get(url) {
+        const response = await fetch(url);
 
-        post(url,data) {
-            return new Promise((resolve , reject) => {
-                fetch(url,{
-                    method:'POST',
-                    headers: {
-                        'Content-type':'application/json'
-                    },
-                    body:JSON.stringify(data);
-                })
-                    .then(res => res.json())
-                    .then(data => resolve(data))
-                    .catch(err => reject(err));
-                });
-            }
-
-            put(url,data) {
-                return new Promise((resolve , reject) => {
-                    fetch(url,{
-                        method:'PUT',
-                        headers: {
-                            'Content-type':'application/json'
-                        },
-                        body:JSON.stringify(data);
-                    })
-                        .then(res => res.json())
-                        .then(data => resolve(data))
-                        .catch(err => reject(err));
-                    });
-                }
-
-            delete(url) {
-                return new Promise((resolve , reject) => {
-                    fetch(url,{
-                            method:'DELETE',
-                            headers: {
-                                'Content-type':'application/json'
-                            }
-                        })
-                            .then(res => res.json())
-                            .then(() => resolve('Resource deleted'))
-                            .catch(err => reject(err));
-                        });
-                    }
-        
-    
-        
-        
+        const resData = await response.json();
+        return resData;
     }
+
+    async post(url,data) {
+         
+        const response = await fetch(url, {
+             method:'POST',
+                headers: {
+                    'Content-type':'application/json'
+                },
+                body : JSON.stringify(data);
+        });
+
+        const resData = await response.json();
+        return resData;
+    }
+
+    async put(url,data) {
+        const response = await fetch(url,{
+            method:'PUT',
+            headers: {
+                    'Content-type':'application/json'
+                },
+           body:JSON.stringify(data);
+         });
+            const resData = await response.json();
+            return resData;
+    }
+
+    async delete(url) {
+       const response = await fetch(url, {
+            method:'DELETE',
+            headers: {
+                'Content-type':'application/json'
+            }
+        });
+        const resData = await 'Resource Deleted';
+        return resData;
+
+    }
+    
+}

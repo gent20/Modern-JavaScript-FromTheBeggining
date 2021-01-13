61a316cf9771cd83194d9da1da38efc5d0635c8d
gent20 <gentkastratio@gmail.com>
2021-01-13 19:34:11 +0100

    initial commit (github finder)

---
+++ easyhttp2/app.js
@@ -0,0 +1,37 @@
+const http = new EasyHTTP;
+
+
+http.get('https://jsonplaceholder.typicode.com/users')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+
+
+
+const data = {
+    name:	"Leanne Graham",
+    username	:"Bret",   
+    email	:"Sincere@april.biz"
+}
+
+http.post('https://jsonplaceholder.typicode.com/users',data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+
+
+http.put('https://jsonplaceholder.typicode.com/users/2',data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+
+
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+
+
+
+
+    

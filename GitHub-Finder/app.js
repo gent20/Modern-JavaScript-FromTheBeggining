dc06c43bced6aeb26df3f74c655f2e36827a453d
gent20 <gentkastratio@gmail.com>
2021-01-13 20:32:02 +0100

    display the profile

--- GitHub-Finder/app.js
+++ GitHub-Finder/app.js
@@ -1,21 +1,30 @@
-const github = new GitHub();
+// Init Github
+const github = new Github;
+// Init UI
+const ui = new UI;
 
+// Search input
 const searchUser = document.getElementById('searchUser');
 
+// Search input event listener
 searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
 
-    const userText = e.target.value;
+  if(userText !== ''){
+   // Make http call
+   github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
 
-    if(userText !== ''){
-        github.getUser(userText)
-            .then(data => {
-                if(data.profile.message === 'Not Found'){
-
-            } else{
-
-            }
-        })
-    } else {
-        
-    }
-});
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+      }
+    })
+  } else {
+    // Clear profile
+    
+  }
+}); 
--- GitHub-Finder/github.js
+++ GitHub-Finder/github.js
@@ -1,17 +1,16 @@
 class Github {
     constructor() {
         this.client_id = '16937441e80d694d5a99';
-        this.client_secret = '009f98322b72c146ed31c6beb14dccb771d7988e'
+        this.client_secret = '009f98322b72c146ed31c6beb14dccb771d7988e';
     }
 
-    async getUser(user){
+    async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
-
+    
         const profile = await profileResponse.json();
-
+    
         return {
-            profile
+          profile
         }
-
+      }
     }
-}
